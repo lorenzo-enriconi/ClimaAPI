31d129a243246d57700ec9781b23234af64dc592
lorenzo-enriconi <lorenzoenriconi@gmail.com>
2026-04-02 08:49:58 -0300

    Voltando ao codigo original para teste

--- main.py
+++ main.py
@@ -16,32 +16,74 @@ API_KEY = "62ec1a83bac5c1c68bd48e9ef82b37c0"
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 BASE_URL_FORECAST = "https://api.openweathermap.org/data/2.5/forecast"
 
-def chamar_api_clima(url: str, cidade: str, parametro_extra: dict | None = None) -> dict:
+@app.get("/api/previsao")
+def obter_previsao(cidade: str):
     parametros = {
         "q": cidade,
         "appid": API_KEY,
         "units": "metric",
         "lang": "pt_br",
-        **(parametro_extra or {})
+        "cnt": 40
     }
     
     try:
-        resposta = requests.get(url, params=parametros, timeout=10, verify=False)
+        resposta = requests.get(BASE_URL_FORECAST, params=parametros, timeout=10, verify=False)
+        return resposta.json()
+    except Exception as e:
+        return {"erro_fatal": str(e)}
+
+
+@app.get("/api/clima")
+def obter_clima(cidade: str):
+
+    parametros = {
+        "q": cidade,
+        "appid": API_KEY,
+        "units": "metric",
+        "lang": "pt_br"
+    }
+  
+    try:
+        resposta = requests.get(BASE_URL, params=parametros, timeout=10, verify=False)
+        # Se a API externa retornar erro (401, 404, etc), isso vai nos mostrar
         if resposta.status_code != 200:
             return {
                 "erro_externo": True,
                 "conteudo_bruto": resposta.text[:200]
             }
+            
         return resposta.json()
+    
     except Exception as e:
-        return {"erro_fatal": str(e)}
-    
-    
-@app.get("/api/previsao")
-def obter_previsao(cidade: str):
-    return chamar_api_clima(BASE_URL, cidade)
+        return{"erro_fatal": str(e)}
 
-
-@app.get("/api/clima")
-def obter_clima(cidade: str):
-    return chamar_api_clima(BASE_URL_FORECAST, cidade, {"cnt": 40})
+#@app.get("/api/previsao")
+#def chamar_api_clima(url: str, cidade: str, parametro_extra: dict | None = None) -> dict:
+#    parametros = {
+#        "q": cidade,
+#        "appid": API_KEY,       
+#        "units": "metric",
+#        "lang": "pt_br",
+#        **(parametro_extra or {})
+#    }
+#    
+#    try:
+#        resposta = requests.get(url, params=parametros, timeout=10, verify=False)
+#        if resposta.status_code != 200:
+#            return {
+#                "erro_externo": True,
+#                "conteudo_bruto": resposta.text[:200]
+#            }
+#        return resposta.json()
+#    except Exception as e:
+#        return {"erro_fatal": str(e)}
+#    
+#    
+#@app.get("/api/previsao")
+#def obter_previsao(cidade: str):
+#    return chamar_api_clima(BASE_URL, cidade)
+#
+#
+#@app.get("/api/clima")
+#def obter_clima(cidade: str):
+#    return chamar_api_clima(BASE_URL_FORECAST, cidade, {"cnt": 40})
